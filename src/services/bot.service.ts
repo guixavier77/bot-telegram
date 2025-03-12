08b4f905b02f4c0e98963857a7a825be6ad70cc0
Guilherme Xavier <xguilherme1@gmail.com>
2025-03-11 21:10:10 -0300

    feat: exibindo mensagem para comandao inexistente

--- src/services/bot.service.ts
+++ src/services/bot.service.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 class BotService {
     private bot: Telegraf;
+		private validCommands: string[];
 
     constructor() {
 			const botToken = process.env.BOT_TOKEN;
@@ -13,6 +14,7 @@ class BotService {
 			}
 
 			this.bot = new Telegraf(botToken);
+			this.validCommands = ["help", "teste", "link"]; 
     }
 
     connect(): void {
@@ -32,16 +34,52 @@ class BotService {
         this.bot.command("teste", (ctx: NarrowedContext<Context, Update.MessageUpdate<Message.TextMessage>>) => {
             this.commandMoedas(ctx);
         });
+
+				this.bot.hears(/\/\w+/, (ctx) => this.handleUnknownCommand(ctx));
     }
+		
 
 		commandHelp(ctx: NarrowedContext<Context, Update.MessageUpdate<Message.TextMessage>>) {
 			ctx.reply(
-					`*Lista de comandos:*\n\n` +
-					`🛠️ \`/help\` \\- *Mostra a lista de comandos do bot*\n` +
-					`🔗 \`/link\` \\- *Gera um link de afiliado*`,
-					{ parse_mode: "MarkdownV2" }
+				`<b>📋 Lista de comandos:</b>\n\n` +
+				`🛠️ <b>/help</b> - <i>Mostra a lista de comandos do bot</i>\n` +
+				`🔗 <b>/link</b> - <i>Gera um link de afiliado</i>\n\n` +
+				`<b>🤖 Bada Bot</b>`,
+				{ parse_mode: "HTML" }
 			);
+		
 		}
+
+		handleUnknownCommand(ctx: NarrowedContext<Context, Update.MessageUpdate<Message.TextMessage>>) {
+			const messageText = ctx.message?.text?.trim();
+
+			console.log(messageText);
+			if (messageText && messageText.startsWith("/")) {
+					const command = messageText.substring(1).toLowerCase();
+					if (!this.validCommands.includes(command)) {
+						ctx.reply(
+							`🚫 <b>Comando não reconhecido!</b>\n\n` +
+							`Parece que o comando <b>/${command}</b> não está disponível no momento.\n\n` +
+							`Por favor, verifique a lista de comandos válidos e tente novamente.\n\n` +
+							`<b>Digite /help para ver os comandos disponíveis. </b>😊\n\n` + 
+							`<b><i>🤖 Bada Bot</i></b>`,
+
+							{ parse_mode: "HTML" }
+					);
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					}
+			}
+	}
 	
 	
 	
