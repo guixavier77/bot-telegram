dee33aed47acbffad6039d1440e980469327e195
Guilherme Xavier <xguilherme1@gmail.com>
2025-03-12 09:55:45 -0300

    fix: controller bot

--- src/controllers/bot.controller.ts
+++ src/controllers/bot.controller.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
 import UsersService from "../services/commandsBot.service";
-const usersService = new UsersService();
 
 export default class BotController {
 
     async connect(req: Request, res: Response): Promise<void> {
         try {
-            const user = await usersService.connect();
+            // const user = await usersService.connect();
         
-            res.status(200).send({ msg: 'Bot is running', user });
+            res.status(200).send({ msg: 'Bot is running'});
         } catch (error) {
             res.status(500).send({msg: error instanceof Error ? error.message : 'Unknown error' });
         }
